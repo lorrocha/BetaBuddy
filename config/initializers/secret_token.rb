9dcd4b33fc85f1b55103b9e255c23da4cd5de38e
Lorry Rocha <sorai120@yahoo.com>
2013-12-27 12:04:33 -0500

    This is the beta-buddy initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetaBuddy::Application.config.secret_key_base = '6f3cbffb6a692ba237876e90e88f1b0c082ab31776ee52fa29f9cd035816c6033995ddb7921194a53258d81aad18f1e6e9c8dbf2ea5fb8f877a673870233b8f4'
