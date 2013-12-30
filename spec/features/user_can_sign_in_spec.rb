401507c4744d02187e2d57fbd821150344cff41a
Lorry Rocha <sorai120@yahoo.com>
2013-12-30 17:21:06 -0500

    Began acceptance test for the user class. Begin working on this again at user_can_sign_in_spec

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -1,1 +1,20 @@
 require 'spec_helper'
+
+feature "A viewer can log in and authenticate", %{
+  As a writer
+  I want to log in to the BetaBuddy Server
+  So that I can get and give feedback on my prose} do
+    # * Site checks if the user is logged in
+    # * If not logged in, site prompts for authenticated username and password
+    # * If username and password does not match any existing account in server, and error is returned
+    # * If username and password error is returned, a link to the Register page is provided
+    # * To register, email, username, and password fields are required
+    # * Once account is registration, validation/welcome email is sent to email provided
+    # * If log in is valid, logs in
+
+  it 'can find the register page' do
+    visit '/users/new'
+    #start here!
+  end
+
+end
