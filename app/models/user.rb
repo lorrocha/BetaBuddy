2f3251d1d2cbb193a7753907d467531c71152fcc
Lorry Rocha <sorai120@yahoo.com>
2013-12-30 16:54:45 -0500

    Added devise to user, theoretically it ought to authenticate now. Yaaay

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :username
   validates_presence_of :encrypted_password
   validates_format_of :email,
