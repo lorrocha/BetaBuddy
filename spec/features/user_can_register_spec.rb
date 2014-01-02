76d25de42b541a16324496cb3adc941eb19a4425
Lorry Rocha <sorai120@yahoo.com>
2014-01-02 11:34:39 -0500

    Added more comprehensive sign in and sign up boundary case tests

---
+++ spec/features/user_can_register_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+feature "A viewer can register to authenticate", %{
+  As a writer
+  I want to log in to the BetaBuddy Server
+  So that I can get and give feedback on my prose} do
+    # * Site checks if the user is logged in
+    # * If not logged in, site prompts for authenticated username and password
+    # * If username and password does not match any existing account in server, and error is returned
+    # * If username and password error is returned, a link to the Register page is provided
+    # * To register, email, username, and password fields are required
+    # * Once account is registration, validation/welcome email is sent to email provided
+    # * If log in is valid, logs in
+
+  it 'can register' do
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'doobee@yahoo.com'
+    fill_in 'Username', with: 'doobs'
+    fill_in 'user_password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+
+    click_button 'Sign up'
+    expect(page).to have_content('Welcome, warrior of the pen!')
+  end
+
+  context 'it does not register' do
+    it 'when it does not include all the fields' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'doobee@yahoo.com'
+      click_button 'Sign up'
+
+      expect(page).to have_content("can't be blank")
+    end
+
+    it 'when password confimation does not match' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'doobee@yahoo.com'
+      fill_in 'Username', with: 'doobs'
+      fill_in 'user_password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345675'
+
+      click_button 'Sign up'
+      expect(page).to have_content("doesn't match")
+    end
+
+    it 'does not register if an account has been made with that email already' do
+      account = FactoryGirl.create(:user)
+
+      visit '/users/sign_up'
+      fill_in 'Email', with: account.email
+      fill_in 'Username', with: 'doobs'
+      fill_in 'user_password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_button 'Sign up'
+
+      expect(page).to have_content('has already been taken')
+    end
+  end
+
+
+
+end
--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -4,42 +4,67 @@ feature "A viewer can register to authenticate", %{
   As a writer
   I want to log in to the BetaBuddy Server
   So that I can get and give feedback on my prose} do
-    # * Site checks if the user is logged in
-    # * If not logged in, site prompts for authenticated username and password
-    # * If username and password does not match any existing account in server, and error is returned
-    # * If username and password error is returned, a link to the Register page is provided
-    # * To register, email, username, and password fields are required
-    # * Once account is registration, validation/welcome email is sent to email provided
-    # * If log in is valid, logs in
 
-  it 'can find the register page' do
-    visit '/users/sign_up'
-    fill_in 'Email', with: 'doobee@yahoo.com'
-    fill_in 'Username', with: 'doobs'
-    fill_in 'Password', with: '12345678', match: :prefer_exact
-    fill_in 'Password confirmation', with: '12345678'
+  scenario "an existing user specifies valid email and password" do
+    user = FactoryGirl.create(:user)
 
-    click_button 'Sign up'
-    expect(page).to have_content('Welcome, warrior of the pen!')
+    visit root_path
+    click_link 'Sign In'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+    expect(page).to have_content('Welcome Back!')
   end
 
-  it 'does not sign in when it does not include all the fields' do
-    visit '/users/sign_up'
-    fill_in 'Email', with: 'doobee@yahoo.com'
-    click_button 'Sign up'
+  context "user is not given a link to sign in if they're already authenticated" do
+    let(:user) {FactoryGirl.create(:user)}
 
-    expect(page).to have_content("can't be blank")
+    def login
+      visit root_path
+      click_link 'Sign In'
+
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+
+      click_button 'Sign In'
+    end
+
+
+    it 'does not show the sign in link on the pages' do
+      login
+      expect(page).to_not have_content('Sign In')
+    end
+
+    it 'sign in cannot be accessed via url' do
+      login
+      visit new_user_session_path
+      expect(page).to have_content("Woops! Looks like you're already signed in!")
+    end
   end
 
-    it 'does not sign in when password confimation does not match' do
-    visit '/users/sign_up'
-    fill_in 'Email', with: 'doobee@yahoo.com'
-    fill_in 'Username', with: 'doobs'
-    fill_in 'Password', with: '12345678', match: :prefer_exact
-    fill_in 'Password confirmation', with: '12345675'
+  context "user is not signed in when the password is wrong" do
+    let(:user) {FactoryGirl.create(:user)}
 
-    click_button 'Sign up'
-    expect(page).to have_content("doesn't match")
+    it 'has the wrong password' do
+      visit root_path
+      click_link 'Sign In'
+
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'bloop'
+      click_button 'Sign In'
+      expect(page).to have_content('Invalid email or password')
+    end
+
+    it 'has the wrong email' do
+      visit root_path
+      click_link 'Sign In'
+
+      fill_in 'Email', with: 'user.email344'
+      fill_in 'Password', with: user.password
+      click_button 'Sign In'
+      expect(page).to have_content('Invalid email or password')
+    end
   end
 
 end
