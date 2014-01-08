3c0f08e32b38168cb2532e87ea8a29ac2c9a4d66
Lorry Rocha <sorai120@yahoo.com>
2014-01-08 12:04:02 -0500

    Added the ability to edit a currently existing prose.

--- app/controllers/proses_controller.rb
+++ app/controllers/proses_controller.rb
@@ -1,17 +1,14 @@
 class ProsesController < ApplicationController
+  before_action :authorize_user, except:[:show, :index]
 
   def new
-    if user_signed_in?
-      set_user
-      @prose = @user.proses.build
-    else
-      redirect_to new_user_session_path, notice: 'Woops! Gotta sign in, first!'
-    end
+      @owner = current_user
+      @prose = @owner.proses.build
   end
 
   def create
-    set_user
-    @prose = @user.proses.build(prose_params)
+    @owner = current_user
+    @prose = @owner.proses.build(prose_params)
     if @prose.save
       redirect_to user_prose_path(@prose), notice: 'Your ink has been committed to paper.'
     else
@@ -19,14 +16,35 @@ class ProsesController < ApplicationController
     end
   end
 
+  def show
+    @prose = Prose.find(params[:id])
+  end
+
+  def edit
+    @prose = Prose.find(params[:id])
+  end
+
+  def update
+    @prose = Prose.find(params[:id])
+    if @prose.update(prose_params)
+      redirect_to "/users/#{@prose.user.id}/proses/#{@prose.id}", notice: "#{@prose.title} has been updated."
+    else
+      render :new
+    end
+  end
+
   private
 
-  def set_user
+  def set_current_user
     @user = current_user
   end
 
   def prose_params
     params.require(:prose).permit(:user_id,:title, :description, :current_state)
+  end
+
+  def authorize_user
+    redirect_to new_user_session_path, notice: 'Woops! Gotta sign in, first!' unless user_signed_in?
   end
 
 end
---
+++ spec/features/user_can_edit_a_prose_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+feature 'The owner of a prose can edit his/her prose',%q{
+    As an authenticated user
+    I want to edit the prose I already uploaded
+    so that I can offer a more polished piece to be critiqued
+} do
+    # Authenticated user can access page via 'Edit' button
+    # If not an authenticated user, redirected to the login page
+    # Converts prose from the browser into a text box (where edits can be freely made)
+    # Optionally can assign one or more than one genre tag to the prose
+    # "Save" button saves the overwrites the original file, replacing its old content
+    # When "Save" is used, browser is redirected to the page displaying the newly editted prose
+
+  let(:molly) {FactoryGirl.create(:user)}
+  let(:fanfic) {FactoryGirl.create(:prose, user_id:molly.id)}
+
+  def sign_in
+    visit new_user_session_path
+    fill_in "Email", with: molly.email
+    fill_in "Password", with: molly.password
+    click_button "Sign In"
+  end
+
+  context 'The user is the owner of the prose' do
+    scenario 'can see the link to edit their prose' do
+      sign_in
+      visit "/users/#{fanfic.user_id}/proses/#{fanfic.id}"
+
+      expect(page).to have_content(fanfic.description)
+      expect(page).to have_content(fanfic.title)
+      expect(page).to have_content(molly.username)
+      expect(page).to have_content('Edit')
+    end
+
+    scenario 'can edit their page' do
+      sign_in
+      visit "/users/#{fanfic.user_id}/proses/#{fanfic.id}"
+      click_on 'Edit'
+
+      title= 'The VERY best story ever!'
+      fill_in "Title", with: title
+      click_button "Update Prose"
+      expect(page).to have_content(title)
+    end
+  end
+
+  context 'The user is not the owner of the prose' do
+    scenario 'can see the link to beta-read the prose' do
+    end
+  end
+
+
+end
