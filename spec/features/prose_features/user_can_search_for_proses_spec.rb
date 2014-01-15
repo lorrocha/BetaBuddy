6d95cc788bbcb77438554226e248afbc999b3443
Lorry Rocha <sorai120@yahoo.com>
2014-01-15 17:56:46 -0500

    Added a bunch of additional tests to make sure the search functionality wont die out because of the filter. Both in the model spec and in the feature branch.

--- spec/features/prose_features/user_can_search_for_proses_spec.rb
+++ spec/features/prose_features/user_can_search_for_proses_spec.rb
@@ -8,6 +8,8 @@ feature 'A visitor can search for a specific story', %q{
 
   let!(:molly) {FactoryGirl.create(:user, username:'woopwoop')}
   let(:fanfic) {FactoryGirl.create(:prose, user_id:molly.id, title:'hi')}
+  let(:genre) {FactoryGirl.create(:genre)}
+  let(:wrong_prose) {FactoryGirl.create(:prose, title:'BLEEP')}
 
   scenario 'A user can search for a prose by author content' do
     Prose.destroy_all
@@ -40,7 +42,32 @@ feature 'A visitor can search for a specific story', %q{
 
     expect(page).to_not have_content('woops')
     expect(page).to have_content('Sorry, there were no matches.')
+  end
 
+  scenario 'A user can search for a prose by genre' do
+    Prose.destroy_all
+    fanfic.prose_tags.build(genre_id:genre.id).save!
+
+    visit '/search'
+    select genre.name, from: 'Genre'
+    click_button 'Search'
+
+    expect(page).to have_content(fanfic.title)
+    expect(page).to_not have_content(wrong_prose.title)
+  end
+
+  scenario 'A user can search for a prose by genre and text' do
+    Prose.destroy_all
+    fanfic.prose_tags.build(genre_id:genre.id).save!
+    wrong_prose.prose_tags.build(genre_id:genre.id).save!
+
+    visit '/search'
+    fill_in 'Search', with: fanfic.title
+    select genre.name, from: 'Genre'
+    click_button 'Search'
+
+    expect(page).to have_content(fanfic.title)
+    expect(page).to_not have_content(wrong_prose.title)
   end
 
 end
--- spec/models/prose_spec.rb
+++ spec/models/prose_spec.rb
@@ -12,10 +12,11 @@ describe Prose do
   it {should have_many(:prose_tags)}
   it {should have_many(:genres)}
 
+  let(:prose) {FactoryGirl.create(:prose)}
+  let!(:genre1) {FactoryGirl.create(:genre)}
+  let!(:genre2) {FactoryGirl.create(:genre)}
+
   it 'Should have multiple genres in relationship to it' do
-    prose = FactoryGirl.create(:prose)
-    genre1 = FactoryGirl.create(:genre)
-    genre2 = FactoryGirl.create(:genre)
 
     ProseTag.create(prose_id:prose.id, genre_id:genre1.id)
     ProseTag.create(prose_id:prose.id, genre_id:genre2.id)
@@ -24,11 +25,36 @@ describe Prose do
 
   it 'can be searched and returned' do
     Prose.destroy_all
+    prose
     tester = 'Dance baby dance!'
-    FactoryGirl.create(:prose)
-    prose = FactoryGirl.create(:prose, title:tester)
+    FactoryGirl.create(:prose, title:tester)
 
     expect(Prose.search(tester, nil).count).to eql(1)
+  end
+
+  it 'can be searched and returned by genre' do
+    Prose.destroy_all
+    FactoryGirl.create(:prose)
+
+    prose.prose_tags.build(genre_id:genre1.id).save!
+    expect(prose.genres).to include(genre1)
+    expect(Prose.count).to eql(2)
+
+    expect(Prose.search(nil, genre1.id).count).to eql(1)
+  end
+
+  it 'can be searched and returned by both text and genre' do
+    Prose.destroy_all
+    title = 'Gone with the wind'
+    FactoryGirl.create(:prose, title: title)
+    test = FactoryGirl.create(:prose, title: title)
+
+    prose.prose_tags.build(genre_id:genre1.id).save!
+    test.prose_tags.build(genre_id:genre1.id).save!
+
+    expect(Prose.search(title, nil).count).to eql(2)
+    expect(Prose.search(nil, genre1.id).count).to eql(2)
+    expect(Prose.search(title, genre1.id).count).to eql(1)
   end
 
 end
